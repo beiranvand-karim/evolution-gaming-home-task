9ccce25221f67c55a8a2f7262b22767fbfab4752
beiranvand.karim <beiranvand.karim@gmail.com>
2018-01-24 11:17:51 +0330

    init commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/first';
+import 'rxjs/add/operator/skip';
+import 'rxjs/add/operator/find';
+import 'rxjs/add/operator/filter';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
